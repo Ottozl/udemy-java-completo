1aef1bf054f0a30a639302dbeccb9b6b8672bc70
Ottozl <ottoa7x@gmail.com>
2021-05-30 19:31:49 -0300

    Aula 142 e bloco finally

--- aula139-try-catch/src/application/Program.java
+++ aula139-try-catch/src/application/Program.java
@@ -7,6 +7,19 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		method1();
+		System.out.println("End of the program");
+
+	}
+	public static void method1() {
+		System.out.println("***Method 1 Starts***");
+		method2();
+		System.out.println("***Method 1 Finishes***");
+
+
+	}
+	public static void method2() {
+		System.out.println("***Method 2 Starts***");
 		Scanner sc = new Scanner(System.in);
 		try {
 		String[] vect = sc.nextLine().split(" ");
@@ -15,12 +28,13 @@ public class Program {
 		}
 		catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("Invalid Position!");
+			e.printStackTrace();
+			sc.next();
 		}
 		catch (InputMismatchException e) {
 			System.out.println("Input error!");
 		}
-		System.out.println("End of the program");
+		System.out.println("***Method 2 Finishes");
 		sc.close();
 	}
-
 }
