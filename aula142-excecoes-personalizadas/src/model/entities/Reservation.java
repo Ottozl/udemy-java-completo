d675d0b8b574e79879f44864525b4b27960584ec
Ottozl <ottoa7x@gmail.com>
2021-10-01 12:55:40 -0300

    Atualizações

--- aula142-excecoes-personalizadas/src/model/entities/Reservation.java
+++ aula142-excecoes-personalizadas/src/model/entities/Reservation.java
@@ -4,7 +4,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
-import models.exceptions.DomainException;
+import model.exceptions.DomainException;
 
 public class Reservation {
 
--- aula142-excecoes-personalizadas/src/model/exceptions/DomainException.java
+++ aula142-excecoes-personalizadas/src/model/exceptions/DomainException.java
@@ -1,4 +1,4 @@
-package models.exceptions;
+package model.exceptions;
 
 public class DomainException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
